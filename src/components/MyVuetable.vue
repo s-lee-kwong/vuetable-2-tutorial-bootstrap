5dd98c7f82517b585d8c538637d2422aba45241c
Shawn Lee-Kwong <splitnova@gmail.com>
2017-10-24 10:06:38 -0400

    tweaks

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -91,14 +91,14 @@ export default {
       ],
       moreParams: {},
       localData: [
-        {name: "Test", email: "Test1", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
-        {name: "Test", email: "Test2", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
-        {name: "Test", email: "Test5", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
-        {name: "Test", email: "Test4", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
-        {name: "Test", email: "Test3", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343434"},
-        {name: "Test", email: "Test0", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343434"},
-        {name: "Test", email: "Test9", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343434"},
-        {name: "Test", email: "Test8", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343431114"},
+        {id: 1, name: "Test", email: "Test1", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
+        {id: 2, name: "Test", email: "Test2", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
+        {id: 3, name: "Test", email: "Test5", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
+        {id: 4, name: "Test", email: "Test4", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "M", salary: "343434"},
+        {id: 5, name: "Test", email: "Test3", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343434"},
+        {id: 6, name: "Test", email: "Test0", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343434"},
+        {id: 7, name: "Test", email: "Test9", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343434"},
+        {id: 8, name: "Test", email: "Test8", age: 25, birthdate: "09-09-1991", nickname: "Blah", gender: "F", salary: "343431114"},
       ],
       filteredData: []
   	}
@@ -154,15 +154,12 @@ export default {
     },
     onChangePage (page) {
       this.$refs.vuetable.changePage(page)
-
-      console.log("CHANGE PAGE" + page)
     },
     onCellClicked (data, field, event) {
-      console.log('cellClicked: ', field.name)
       this.$refs.vuetable.toggleDetailRow(data.id)
     },
     onAction (action, data, index) {
-      console.log('custom-actions: ' + action, data.name, index)
+      console.log('slot-actions: ' + action, data.name, index)
     },
     onFilterSet (filterText) {
       this.moreParams = {
